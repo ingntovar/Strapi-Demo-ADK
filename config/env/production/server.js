eac4238ff5406c625b9b44c26f15845b1fecc14c
Nicolas Tovar <nicolas.tovar@projekt202.com>
2022-06-15 12:01:21 -0500

    update server.js on prod

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   proxy: true,
   url: env('MY_HEROKU_URL'),
-  // app: { 
-  //   keys: env.array('APP_KEYS')
-  // },
+  app: { 
+    keys: env.array('APP_KEYS')
+  },
 })
