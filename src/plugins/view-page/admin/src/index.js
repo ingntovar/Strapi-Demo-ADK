c3905aaef04d8b54c39ddbd4df9737f1282b062e
Nicolas Tovar <nicolas.tovar@projekt202.com>
2022-06-21 15:30:41 -0500

    changing link menu name

--- src/plugins/view-page/admin/src/index.js
+++ src/plugins/view-page/admin/src/index.js
@@ -14,7 +14,7 @@ export default {
       icon: PluginIcon,
       intlLabel: {
         id: `${pluginId}.plugin.name`,
-        defaultMessage: name,
+        defaultMessage: 'View Url Plugin',
       },
       Component: async () => {
         const component = await import(/* webpackChunkName: "[request]" */ './pages/App');
